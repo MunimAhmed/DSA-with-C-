ea8acdb3c0f5291474b6b0e73b3cdf2c172e3a7d
MunimAhmed <munim.ah018@gmail.com>
2022-09-03 23:54:56 +0600

    stack full operations

--- stack.cpp
+++ stack.cpp
@@ -1,11 +1,74 @@
-#include<bits/stdc++.h>
+#include<iostream>
 
 using namespace std;
+
+#define n 100
+
+class stack{
+    int* arr;
+    int top;
+
+    public:
+    stack(){
+        arr=new int[n];
+        top=-1;
+    }
+
+    void push(int x){
+
+        if(top==n-1){
+            cout<<"Stack overflow"<<endl;
+            return;
+        }
+
+        top++;
+        arr[top]=x;
+    }
+
+    void pop(){
+        if(top==-1)
+        {
+            cout<<"No element to pop"<<endl;
+            return;
+        }
+
+        top--;
+    }
+
+    int Top(){
+        if(top==-1)
+        {
+            cout<<"No element in stack"<<endl;
+            return -1;
+        }
+        return arr[top];
+
+    }
+
+    bool empty()
+    {
+        return top==-1;
+    }
+};
+
 
 
 int main()
 {
-
-cout<<"HELLO"<<endl;
+stack m;
+m.push(1);
+m.push(2);
+m.push(3);
+m.push(4);
+m.push(5);
+cout<<m.Top()<<endl;
+m.pop();
+cout<<m.Top()<<endl;
+m.pop();
+m.pop();
+m.pop();
+m.pop();
+m.pop();
+cout<<m.empty()<<endl;
 return 0;
 }
